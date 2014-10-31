f01d44327f19fd9749852c547c4bbe7eeb8af755
Thiago Scalone <thiago@scalone.com.br>
2014-10-31 19:38:12 -0200

    Implement interface to deal with GPRS and WIFI, if Network.media is init by GPRS works with Network::GPRS, if is WIFI Network:Wifi.

--- mrblib/network.rb
+++ mrblib/network.rb
@@ -1,10 +1,27 @@
 class Network
   class << self
-    attr_accessor :pass, :user, :apn
+    attr_accessor :interface
   end
 
-  def self.getaddress(host)
-    TCPSocket.getaddress(host)
+  def self.ping(host, timeout)
+    _ping TCPSocket.getaddress(host), timeout
+  end
+
+  def self.init(media, options)
+    if media == :gprs
+      @interface = Network::Gprs
+    else
+      @interface = Network::Wifi
+    end
+    @interface.init(options)
+  end
+
+  def self.method_missing(method, *args, &block)
+    if @interface.respond_to? :method
+      @interface.send(method, *args, &block)
+    else
+      super
+    end
   end
 end
 
