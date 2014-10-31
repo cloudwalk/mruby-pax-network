a61434425ac7876e9d00511b72dbc9f0b00be8dc
Thiago Scalone <thiago@scalone.com.br>
2014-10-31 19:36:53 -0200

    Implemented Network::Wifi and wifi.c

---
+++ mrblib/wifi.rb
@@ -0,0 +1,44 @@
+class Network
+  class Wifi
+    class << self
+      attr_accessor :bssid, :essid, :rssi, :authentication, :password, :essid, :bssid, :channel, :mode, :cipher
+      attr_reader :media
+      #@media = Network::MEDIA_WIFI
+      @media = :wifi
+    end
+
+    AUTH_NONE_OPEN       = 1
+    AUTH_NONE_WEP        = 2
+    AUTH_NONE_WEP_SHARED = 3
+    AUTH_IEEE8021X       = 4
+    AUTH_WPA_PSK         = 5
+    AUTH_WPA_EAP         = 6
+    AUTH_WPA_WPA2_PSK    = 7
+    AUTH_WPA_WPA2_EAP    = 8
+    AUTH_WPA2_PSK        = 9
+    AUTH_WPA2_EAP        = 10
+
+    PARE_CIPHERS_NONE   = 0
+    PARE_CIPHERS_WEP64  = 1
+    PARE_CIPHERS_WEP128 = 2
+    PARE_CIPHERS_WEPX   = 04
+    PARE_CIPHERS_CCMP   = 10
+    PARE_CIPHERS_TKIP   = 20
+
+    MODE_IBSS    = 1
+    MODE_STATION = 0
+
+    authentications = {
+      :open         => AUTH_NONE_OPEN,
+      :wep_none     => AUTH_NONE_WEP,
+      :wep_shared   => AUTH_NONE_WEP_SHARED,
+      :iEEE8021X    => AUTH_IEEE8021X,
+      :wpa_psk      => AUTH_WPA_PSK,
+      :wpa_eap      => AUTH_WPA_EAP,
+      :wpa_wpa2_psk => AUTH_WPA_WPA2_PSK,
+      :wpa_wpa2_eap => AUTH_WPA_WPA2_EAP,
+      :wpa2_psk     => AUTH_WPA2_PSK,
+      :wpa2_eap     => AUTH_WPA2_EAP
+    }
+  end
+end
---
+++ src/wifi.c
@@ -0,0 +1,176 @@
+/*
+** network.c - Network module
+**
+** Network.init(GPRS, apn: claro.com.br, user: claro.com.br, pass: claro.com.br)
+**   WlSwitchPower(1);
+**   WlInit(NULL);
+** Network.power true/false
+**   WlSwitchPower(1);
+** Network.connect(block = true/false)
+**   WlPppLogin(sAPN,sUser,sPwd,0xff,0,0);
+** Network.connected?
+**   WlPppCheck();
+** Network.disconnect
+**   WlPppLogout();
+**   WlSwitchPower(0);
+**
+** Network.apn, user, pass, type
+**
+** See Copyright Notice in mruby.h
+*/
+
+#include "mruby.h"
+#include <stddef.h>
+#include <string.h>
+
+#include "mruby/array.h"
+#include "mruby/class.h"
+#include "mruby/data.h"
+#include "mruby/hash.h"
+#include "mruby/string.h"
+#include "mruby/variable.h"
+
+#if MRUBY_RELEASE_NO < 10000
+  #include "error.h"
+#else
+  #include "mruby/error.h"
+#endif
+
+#include "osal.h"
+
+static mrb_value
+mrb_wifi_init(mrb_state *mrb, mrb_value klass)
+{
+  mrb_value options, value;
+
+  mrb_get_args(mrb, "|o", &options);
+
+  if (mrb_hash_p(options)) {
+    value = mrb_hash_get(mrb, options, mrb_symbol_value(mrb_intern_cstr(mrb, "essid")));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@essid"), value);
+
+    value = mrb_hash_get(mrb, options, mrb_symbol_value(mrb_intern_cstr(mrb, "bssid")));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@bssid"), value);
+
+    value = mrb_hash_get(mrb, options, mrb_symbol_value(mrb_intern_cstr(mrb, "mode")));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@mode"), value);
+
+    value = mrb_hash_get(mrb, options, mrb_symbol_value(mrb_intern_cstr(mrb, "authentication")));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@authentication"), value);
+
+    value = mrb_hash_get(mrb, options, mrb_symbol_value(mrb_intern_cstr(mrb, "cipher")));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@cipher"), value);
+
+    value = mrb_hash_get(mrb, options, mrb_symbol_value(mrb_intern_cstr(mrb, "password")));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@password"), value);
+  }
+
+  /*return mrb_fixnum_value(OsWlInit(NULL));*/
+  return mrb_fixnum_value(OsWifiOpen());
+}
+
+//TODO Scalone: Check if Power is necessary before init
+static mrb_value
+mrb_wifi_power(mrb_state *mrb, mrb_value klass)
+{
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_fixnum_value(OsWifiSwitchPower(state));
+}
+
+//TODO Scalone: Support blocking and non blocking connection with timeout modification
+static mrb_value
+mrb_wifi_connect(mrb_state *mrb, mrb_value klass)
+{
+  mrb_value password, essid, bssid, channel, mode, authentication, cipher;
+  const char *sPassword, *sEssid, *sBssid;
+
+  ST_WifiApSet wifiSet;
+  WPA_PSK_KEY psk;
+
+  /*memset(psk, 0, sizeof(psk));*/
+  /*memset(wifiSet, 0, sizeof(wifiSet));*/
+
+  /*int AuthMode; [> Authentication modes <]*/
+  authentication = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@authentication"));
+  wifiSet.AuthMode = mrb_fixnum(authentication);
+
+  password = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@password"));
+  sPassword  = mrb_str_to_cstr(mrb, password);
+
+  /*WPA_PSK_KEY PskKey; [> For wpa,wpa2-psk authentication <]*/
+  if (wifiSet.AuthMode == AUTH_NONE_WEP && wifiSet.AuthMode == AUTH_NONE_WEP_SHARED) {
+    strcpy((char *)&psk.Key, sPassword);
+    psk.KeyLen = strlen(sPassword);
+    wifiSet.KeyUnion.PskKey = psk;
+  }
+
+  /*char Essid[33]; [> AP name, it can support 32 bytes at most, and ending with '\0' <]*/
+  essid = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@essid"));
+  sEssid  = mrb_str_to_cstr(mrb, essid);
+  strcpy((char *)&wifiSet.Essid, sEssid);
+
+  /*char Bssid[20]; [> MAC address, if there is no any APs with the same ESSID, Bssid can be "\0"<]*/
+  bssid = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@bssid"));
+  sBssid = mrb_str_to_cstr(mrb, bssid);
+  strcpy((char *)&wifiSet.Bssid, sBssid);
+
+  /*int Channel;  [> Channel, 0:Auto set <]*/
+  channel = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@channel"));
+  wifiSet.Channel = mrb_fixnum(channel);
+
+  /*int Mode; [> Connection mode, 0:Station; 1:IBSS <]*/
+  mode = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@mode"));
+  wifiSet.Mode = mrb_fixnum(mode);
+
+  cipher = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@cipher"));
+  wifiSet.SecMode = mrb_fixnum(cipher);
+
+  /*WlPppLogin(sAPN, sUser, sPass, auth, timeout, keep_alive);*/
+  /*OsWlLogin(sAPN, sUser, sPass, auth, timeout, keep_alive, NULL);*/
+
+  return mrb_fixnum_value(OsWifiConnect(&wifiSet, 1000));
+}
+
+//   0 -> Sucess
+//   1 -> In Progress
+// < 0 -> Fail
+static mrb_value
+mrb_wifi_connected_m(mrb_state *mrb, mrb_value klass)
+{
+  char *sEssid, *sBssid;
+  mrb_int *iRssi, ret;
+
+  ret = OsWifiCheck(sEssid, sBssid, iRssi);
+
+  if (ret == RET_OK) {
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@essid"), mrb_str_new_cstr(mrb, sEssid));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@bssid"), mrb_str_new_cstr(mrb, sBssid));
+    mrb_cv_set(mrb, klass, mrb_intern_lit(mrb, "@rssi"), mrb_fixnum_value(iRssi));
+  }
+
+  return mrb_fixnum_value(ret);
+}
+
+static mrb_value
+mrb_wifi_disconnect(mrb_state *mrb, mrb_value klass)
+{
+  return mrb_fixnum_value(OsWifiDisconnect());
+}
+
+void
+mrb_init_wifi(mrb_state* mrb)
+{
+  struct RClass *network, *wifi;
+
+  network = mrb_define_class(mrb, "Network", mrb->object_class);
+  wifi    = mrb_define_class(mrb, "Wifi", network);
+
+  mrb_define_class_method(mrb, wifi, "init", mrb_wifi_init, MRB_ARGS_REQ(1)|MRB_ARGS_OPT(1));
+  mrb_define_class_method(mrb, wifi, "power", mrb_wifi_power, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, wifi, "connect", mrb_wifi_connect, MRB_ARGS_OPT(1));
+  mrb_define_class_method(mrb, wifi, "connected?", mrb_wifi_connected_m, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, wifi, "disconnect", mrb_wifi_disconnect, MRB_ARGS_NONE());
+}
+
