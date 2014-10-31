bed012b0b0acf4483127656df9d0553e6293d215
Thiago Scalone <thiago@scalone.com.br>
2014-10-31 19:38:36 -0200

    Add init_gprs, init_wifi to mgem initialization.

--- src/mruby_pax_network_gem.c
+++ src/mruby_pax_network_gem.c
@@ -1,7 +1,8 @@
 #include "mruby.h"
 
 void mrb_init_network(mrb_state *mrb);
-void mrb_init_socket(mrb_state *mrb);
+void mrb_init_gprs(mrb_state *mrb);
+void mrb_init_wifi(mrb_state *mrb);
 
 #define DONE mrb_gc_arena_restore(mrb, 0)
 
@@ -9,7 +10,8 @@ void
 mrb_mruby_pax_network_gem_init(mrb_state* mrb)
 {
   mrb_init_network(mrb); DONE;
-  mrb_init_socket(mrb); DONE;
+  mrb_init_gprs(mrb); DONE;
+  mrb_init_wifi(mrb); DONE;
 }
 
 void
