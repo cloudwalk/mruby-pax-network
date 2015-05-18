e09b576a93be31a97f71adb4a5326eb361f6f6fc
Thiago Scalone <thiago@scalone.com.br>
2015-05-18 15:51:15 -0300

    remove white spaces.

--- mrblib/ethernet.rb
+++ mrblib/ethernet.rb
@@ -17,16 +17,16 @@ class Network
     end
 
     # @brief Get access to module
-    # 
+    #
     # @retval RET_OK Success.
     # @retval ERR_DEV_NOT_EXIST Fail to access device.
     # @retval ERR_DEV_BUSY Already in use.
     def self.start; self._start; end
 
     # @brief Sets module into the state of power-on and power-off.
-    # 
+    #
     # @param state [int]: 1 - power-on; 0 - power-off
-    # 
+    #
     # @retval RET_OK Success.
     # @retval ERR_INVALID_PARAM Invalid parameter.
     # @retval ERR_DEV_NOT_EXIST Fail to access device.
@@ -34,25 +34,25 @@ class Network
     def self.power(state); self._power(state); end
 
     # @brief Start connection process.
-    # 
+    #
     # @retval RET_OK Success.
     # @retval RET_CONNECTING During connection process.
     # @retval ERR_INVALID_PARAM Invalid parameter.
-    # @retval ERR_NOT_CONNECT Fail to connect. 
+    # @retval ERR_NOT_CONNECT Fail to connect.
     # @retval ERR_DEV_NOT_OPEN Driver loading exception or module error.
     def self.connnect; self._connnect; end
 
     # @brief Check is connected
-    # 
+    #
     # @retval RET_OK Success.
     # @retval RET_CONNECTING During connection process.
     # @retval ERR_INVALID_PARAM Invalid parameter.
-    # @retval ERR_NOT_CONNECT Fail to connect. 
+    # @retval ERR_NOT_CONNECT Fail to connect.
     # @retval ERR_DEV_NOT_OPEN Driver loading exception or module error.
     def self.connnected?; self._connnected?; end
 
     # @brief Perform disconnect
-    # 
+    #
     # @retval RET_OK Success.
     # @retval ERR_DEV_NOT_OPEN Driver loading exception or module error.
     def self.disconnect; self._disconnect; end
