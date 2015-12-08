b35528b175d9be4b6b35410a75e7e4033c1029c0
Thiago Scalone <thiago@scalone.com.br>
2015-12-08 12:17:34 -0200

    Add mrb_init_ethernet signature on mruby_pax_network_gem.c to avoid warning.

--- src/mruby_pax_network_gem.c
+++ src/mruby_pax_network_gem.c
@@ -3,6 +3,7 @@
 void mrb_init_network(mrb_state *mrb);
 void mrb_init_gprs(mrb_state *mrb);
 void mrb_init_wifi(mrb_state *mrb);
+void mrb_init_ethernet(mrb_state *mrb);
 
 #define DONE mrb_gc_arena_restore(mrb, 0)
 
